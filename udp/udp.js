c1646b9c4c7150298747bdb8bdb33ab2c380b719
Kritidipto Ghosh <kritidipto.ghosh@walmart.com>
2026-06-05 17:42:44 +0530

    fix(): UDP added

--- udp/udp.js
+++ udp/udp.js
@@ -3,7 +3,7 @@ import dgram from "dgram"
 const server = dgram.createSocket("udp4")
 
 server.on("message", (msg, rinfo) => {
-  console.log("data received:", msg)
+  console.log("data received from", rinfo.address + ":" + rinfo.port, msg)
   console.log(msg.toString())
 
   server.send("You sent: " + msg.toString(), rinfo.port, rinfo.address)
@@ -14,3 +14,5 @@ server.on("listening", () => {
 })
 
 server.bind(8080)
+
+//echo  "hello from nc" | nc -u -w1 127.0.0.1 8080
---
+++ udp/udpClient.js
@@ -0,0 +1,22 @@
+import dgram from "dgram"
+
+const client = dgram.createSocket("udp4")
+
+client.bind(0, () => {
+  console.log("My address:", client.address())
+  //echo  "hello from nc" | nc -u -w1 127.0.0.1 $port
+})
+
+client.on("message", (msg, rinfo) => {
+  console.log("Server replied:", msg.toString())
+  console.log("Server address:", rinfo.address + ":" + rinfo.port)
+  // client.close()
+})
+
+setInterval(() => {
+  const message = "ping"+Date.now()
+  client.send(message, 8080, "localhost", (err) => {
+    console.log("Sent:", message)
+  })
+}, 1000)
+
